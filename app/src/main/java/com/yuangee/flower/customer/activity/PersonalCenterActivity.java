10b815dfa9c077e409dd4d19ecf1d8c22c1cae6c
developerLzh <developerlzh@gmai.com>
2017-10-09 11:48:15 +0800

    个人中兴各种事件

--- app/src/main/java/com/yuangee/flower/customer/activity/PersonalCenterActivity.java
+++ app/src/main/java/com/yuangee/flower/customer/activity/PersonalCenterActivity.java
@@ -1,13 +1,36 @@
 package com.yuangee.flower.customer.activity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+import com.luck.picture.lib.PictureSelector;
+import com.luck.picture.lib.config.PictureConfig;
+import com.luck.picture.lib.config.PictureMimeType;
+import com.luck.picture.lib.entity.LocalMedia;
 import com.yuangee.flower.customer.R;
 import com.yuangee.flower.customer.base.RxBaseActivity;
+import com.yuangee.flower.customer.util.StringUtils;
+
+import java.util.List;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * Created by developerLzh on 2017/8/22 0022.
@@ -17,6 +40,117 @@ public class PersonalCenterActivity extends RxBaseActivity {
     @BindView(R.id.toolbar)
     Toolbar toolbar;
 
+    @OnClick(R.id.pick_photo)
+    void pickPhoto() {
+        choosePic(1, 1);
+    }
+
+    @BindView(R.id.ic_photo)
+    ImageView icPhoto;
+
+    @BindView(R.id.person_name)
+    TextView personName;
+
+    @BindView(R.id.person_gender)
+    TextView personGender;
+
+    @BindView(R.id.person_phone)
+    TextView personPhone;
+
+    @BindView(R.id.person_email)
+    TextView personEmail;
+
+    @BindView(R.id.person_consignee)
+    TextView personConsignee;
+
+    @BindView(R.id.person_consignee_phone)
+    TextView personConsigneePhone;
+
+    @BindView(R.id.person_consignee_place)
+    TextView personConsigneePlace;
+
+    @BindView(R.id.person_consignee_method)
+    TextView personConsigneeMethod;
+
+    private AlertDialog dialog;
+
+    @OnClick(R.id.pick_name)
+    void pickName() {
+        showEditDialog(personName,"姓名");
+    }
+
+    @OnClick(R.id.pick_gender)
+    void pickGender() {
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View view = inflater.inflate(R.layout.gender_select_layout, null);
+        RadioButton maleRadio = view.findViewById(R.id.male_radio);
+        RadioButton femaleRadio = view.findViewById(R.id.female_radio);
+        if (personGender.getText().toString().equals("男")) {
+            maleRadio.setChecked(true);
+        } else {
+            femaleRadio.setChecked(true);
+        }
+        maleRadio.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    personGender.setText("男");
+                    dialog.dismiss();
+//                    updateMemberInfo(null, null, "1");
+                }
+            }
+        });
+        femaleRadio.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    personGender.setText("女");
+                    dialog.dismiss();
+//                    updateMemberInfo(null, null, "0");
+                }
+            }
+        });
+        dialog = new AlertDialog.Builder(this)
+                .setTitle("性别")
+                .setView(view)
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .create();
+        dialog.show();
+    }
+
+    @OnClick(R.id.pick_phone)
+    void pickPhone() {
+        showEditDialog(personPhone,"手机号码");
+    }
+
+    @OnClick(R.id.pick_email)
+    void pickEmail(){
+        showEditDialog(personEmail,"邮箱");
+    }
+
+    @OnClick(R.id.pick_consignee)
+    void pickConsignee(){
+        showEditDialog(personConsignee,"收货人姓名");
+    }
+
+    @OnClick(R.id.pick_consignee_phone)
+    void pickConsigneePhone(){
+        showEditDialog(personConsigneePhone,"收货人电话");
+    }
+
+    @OnClick(R.id.person_consignee_method)
+    void pickConsigneeMethod(){
+        showEditDialog(personConsigneeMethod,"收货方式");
+    }
+    @OnClick(R.id.person_consignee_place)
+    void pickConsigneePlace(){
+        showEditDialog(personConsigneePlace,"收货地址");
+    }
 
     @Override
     public int getLayoutId() {
@@ -43,5 +177,61 @@ public class PersonalCenterActivity extends RxBaseActivity {
             finish();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == PictureConfig.CHOOSE_REQUEST) {
+                List<LocalMedia> images = PictureSelector.obtainMultipleResult(data);
+                if (images != null && images.size() > 0) {
+                    String path = images.get(0).getCutPath();
+                    RequestOptions options = new RequestOptions()
+                            .centerCrop()
+                            .placeholder(R.drawable.ic_default_photo_gray)
+                            .diskCacheStrategy(DiskCacheStrategy.ALL);
+                    Glide.with(PersonalCenterActivity.this)
+                            .load(path)
+                            .apply(options)
+                            .into(icPhoto);
+                }
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void showEditDialog(final TextView showView, String hint){
+        final EditText editName = new EditText(this);
+        String sName = showView.getText().toString();
+        editName.setText(sName);
+        if (!StringUtils.isEmpty(sName)) {
+            editName.setSelection(sName.length());
+        }
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        layoutParams.setMargins(50, 20, 50, 10);
+        editName.setLayoutParams(layoutParams);
+        LinearLayout linearLayout = new LinearLayout(this);
+        linearLayout.addView(editName);
+        dialog = new AlertDialog.Builder(this)
+                .setTitle(hint)
+                .setPositiveButton("好", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String s = editName.getText().toString();
+                        showView.setText(s);
+                        dialog.dismiss();
+                    }
+                })
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .setView(linearLayout)
+                .create();
+        dialog.show();
     }
 }
--- app/src/main/java/com/yuangee/flower/customer/base/RxBaseActivity.java
+++ app/src/main/java/com/yuangee/flower/customer/base/RxBaseActivity.java
@@ -2,7 +2,11 @@ package com.yuangee.flower.customer.base;
 
 import android.os.Bundle;
 
+import com.luck.picture.lib.PictureSelector;
+import com.luck.picture.lib.config.PictureConfig;
+import com.luck.picture.lib.config.PictureMimeType;
 import com.trello.rxlifecycle.components.support.RxAppCompatActivity;
+import com.yuangee.flower.customer.activity.PersonalCenterActivity;
 
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
@@ -89,5 +93,34 @@ public abstract class RxBaseActivity extends RxAppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         bind.unbind();
+    }
+
+    protected void choosePic(int x, int y) {
+        // 进入相册 以下是例子：用不到的api可以不写
+        PictureSelector.create(RxBaseActivity.this)
+                .openGallery(PictureMimeType.ofImage())//全部.PictureMimeType.ofAll()、图片.ofImage()、视频.ofVideo()
+                .maxSelectNum(1)// 最大图片选择数量 int
+                .minSelectNum(1)// 最小选择数量 int
+                .imageSpanCount(4)// 每行显示个数 int
+                .selectionMode(PictureConfig.SINGLE)// 多选 or 单选 PictureConfig.MULTIPLE or PictureConfig.SINGLE
+                .isCamera(true)// 是否显示拍照按钮 true or false
+                .isZoomAnim(true)// 图片列表点击 缩放效果 默认true
+                .sizeMultiplier(0.5f)// glide 加载图片大小 0~1之间 如设置 .glideOverride()无效
+                .setOutputCameraPath("/emdriver")// 自定义拍照保存路径,可不填
+                .enableCrop(true)// 是否裁剪 true or false
+                .compress(true)// 是否压缩 true or false
+                .compressMode(PictureConfig.LUBAN_COMPRESS_MODE)//系统自带 or 鲁班压缩 PictureConfig.SYSTEM_COMPRESS_MODE or LUBAN_COMPRESS_MODE
+                .withAspectRatio(x, y)// int 裁剪比例 如16:9 3:2 3:4 1:1 可自定义
+                .hideBottomControls(false)// 是否显示uCrop工具栏，默认不显示 true or false
+                .isGif(false)// 是否显示gif图片 true or false
+                .freeStyleCropEnabled(true)// 裁剪框是否可拖拽 true or false
+                .circleDimmedLayer(false)// 是否圆形裁剪 true or false
+                .showCropFrame(true)// 是否显示裁剪矩形边框 圆形裁剪时建议设为false   true or false
+                .showCropGrid(true)// 是否显示裁剪矩形网格 圆形裁剪时建议设为false    true or false
+                .openClickSound(false)// 是否开启点击声音 true or false
+                .previewEggs(false)// 预览图片时 是否增强左右滑动图片体验(图片滑动一半即可看到上一张是否选中) true or false
+                .rotateEnabled(true) // 裁剪是否可旋转图片 true or false
+                .scaleEnabled(true)// 裁剪是否可放大缩小图片 true or false
+                .forResult(PictureConfig.CHOOSE_REQUEST);//结果回调onActivityResult code
     }
 }
